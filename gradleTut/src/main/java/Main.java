cd268b72cbe182adc32cac5e6f161eb0c37e6164
stadibo <jesperleonard@gmail.com>
2018-11-06 16:07:04 +0200

    exercise 1: gradle project created

---
+++ gradleTut/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import ohtu.Multiplier;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+        System.out.println("who are you? ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+    }
+}
